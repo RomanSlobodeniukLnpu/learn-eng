31498c1c78f5b833253307422790f11da7d410b3
Roman Slobodeniuk <roman.slobodeniuk.mitup.2022@lpnu.ua>
2023-02-12 22:22:48 +0200

    project initialization

---
+++ src/test/java/eng/CodingCityApplicationTests.java
@@ -0,0 +1,12 @@
+package eng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingCityApplicationTests {
+	@Test
+	void contextLoads() {
+
+	}
+}
